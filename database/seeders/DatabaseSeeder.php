831799b4f0a291128b1e721247566135cb3aaeb3
Reannn22 <reyhan.123140022@student.itera.ac.id>
2025-09-03 15:35:04 +0700

    fix seed error

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,12 +11,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // Kalau masih mau seed user manual juga boleh
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'username' => 'testuser',
-            'email' => 'test@example.com',
+        $this->call([
+            UserSeeder::class,
+            AttendanceSeeder::class,
+            TugasSeeder::class,
         ]);
     }
 }
